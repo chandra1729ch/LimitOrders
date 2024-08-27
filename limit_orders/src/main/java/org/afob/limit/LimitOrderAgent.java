3fb6010446e2c8332a96634a7f531d65364a39fa
Tripuravaram Chandrasekhar Reddy <62606087+chandra1729ch@users.noreply.github.com>
2024-08-27 12:32:22 +0530

    Update LimitOrderAgent.java

--- limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
+++ limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
@@ -4,16 +4,35 @@ import org.afob.execution.ExecutionClient;
 import org.afob.prices.PriceListener;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class LimitOrderAgent implements PriceListener {
-
+    private ExecutionClient executionClient;
     public LimitOrderAgent(final ExecutionClient ec) {
+        this.executionClient = ec;
     }
 
 
     @Override
-    public void priceTick(String productId, BigDecimal price) {
+    public void priceTick(String productId, BigDecimal price) throws ExecutionClient.ExecutionException {
+        if(productId.equals("IBM")) {
+            this.addOrder(true, productId,price, 100);
+        }
 
+    }
+
+    public void addOrder(boolean buyFlag, String productId, BigDecimal price, int limit) throws ExecutionClient.ExecutionException{
+        if(buyFlag && price.compareTo(BigDecimal.valueOf(limit)) <  0) {
+            this.executionClient.buy(productId,price.multiply(BigDecimal.valueOf(1000)).setScale(0, RoundingMode.HALF_UP).intValue());
+        } else {
+            throw new ExecutionClient.ExecutionException("Price is More");
+        }
+    }
+
+    public static void main(String[] args) throws ExecutionClient.ExecutionException {
+        ExecutionClient client = new ExecutionClient();
+        LimitOrderAgent limitOrderAgent = new LimitOrderAgent(client);
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(101));
     }
 
 }
